4857ac9efe8052c2c939d88c384ab6fe6e580cbb
Brent Andrew Hendricks <brent.hendricks@p5software.com>
2022-09-19 11:04:22 -0400

    Corrected a defect where the setting was opposite

--- main.py
+++ main.py
@@ -519,8 +519,8 @@ def setup():
             raise Exception ("mongoDb object is missing from settings.json")
 
         if "enabled" not in settings['mongoDb']:
-            settings['mongoDb']['enabled'] = True
-            logger.info("mongoDb -> enabled is missing in the settings file; MongoDB persistence will be enabled.")
+            settings['mongoDb']['enabled'] = False
+            logger.info("mongoDb -> enabled is missing in the settings file; MongoDB persistence will be disabled.")
             
         if "uri" not in settings['mongoDb']:
             raise Exception ("Missing mongoDb -> uri in settings.json")
